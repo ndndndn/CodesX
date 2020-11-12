6ada717e908bf7c4b2fda3dfc9a599a94b9d75df
ndndndn <35729871+ndndndn@users.noreply.github.com>
2020-11-13 03:43:33 +0800

    Create install.sh

---
+++ t/install.sh
@@ -0,0 +1,18 @@
+ 
+#/bin/bash
+apt-get update
+
+apt-get install apache2-utils squid3 -y
+
+# You can edit this line to parsing auth from params or user input
+htpasswd -b -c /etc/squid/passwd USERNAME PASSWORD
+
+wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/paradoxicalx/squid3/master/squid.conf --no-check-certificate
+
+touch /etc/squid/blacklist.acl
+
+service squid restart
+
+update-rc.d squid defaults
+
+echo DONE
