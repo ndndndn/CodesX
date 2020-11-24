cc6dfe0849fa783bea41772b52fcd99f5bef76f7
ndndndn <35729871+ndndndn@users.noreply.github.com>
2020-11-24 23:32:14 +0800

    Create p3.sh

---
+++ p3.sh
@@ -0,0 +1,12 @@
+apt-get -y install aptitude curl
+
+if [ "$IP" = "" ]; then
+        IP=$(curl -s ifconfig.me)
+fi
+
+aptitude -y install squid3
+rm -f /etc/squid3/squid.conf
+
+wget -P /etc/squid3/ "https://raw.githubusercontent.com/paradoxicalx/squid3/master/squid.conf"
+sed -i "s/ipserver/$IP/g" /etc/squid3/squid.conf
+service squid3 restart
